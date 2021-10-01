e4f34d723d7b401ab3c0d78fbbb199f24a50463a
Lautaro Rodriguez <lautarodriguez17@gmail.com>
2021-10-01 15:32:41 -0300

    the app states hace already been created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [linea1, setLinea1] = useState('');
+  const [linea2, setLinea2] = useState('');
+  const [image, setImage] = useState('');
+
+  const onchange = (valor) => {
+    setLinea1(valor.target.value)
+  }
+  const onchange2 = (valor) => {
+    setLinea2(valor.target.value)
+  }
+
+  const onchangeImage = (valor) => {
+    setImage(valor.target.value)
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <select onChange={onchangeImage}>
+          <option value="baby">Baby</option>
+          <option value="dog">Dog</option>
+          <option value="tea">Tea</option>
+          <option value="tellme">Tell Me</option>
+          <option value="why">Why?</option>
+          <option value="you">You!</option>
+        </select>
+        <br />
+        <input type="text" onChange={onchange}/> <br />
+        <input type="text" onChange={onchange2}/> <br />
+        <button>Export</button>
+
+        <div>
+          <span>{linea1}</span><br />
+          <span>{linea2}</span> <br />
+          <img src={"/img/" + image + ".jpg"}></img>
+        </div>
     </div>
   );
 }
